a7b7dd4728835ce543753c298e908032812e6da1
Hyemin Choi <90095211+Hyemin-12@users.noreply.github.com>
2022-02-23 10:39:13 +0900

    Add files via upload

---
+++ WhileExam.java
@@ -0,0 +1,27 @@
+
+public class WhileExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int i = 0;
+//		
+//		while(i < 10) {
+//			System.out.println(i);
+//			i++; // i = i + 1;
+//		}
+		
+		int total = 0;
+		int i = 1;
+		while(i < 100) {
+			total += i;
+			i++;
+		}
+		
+		System.out.println(total);
+		
+		while(true) {
+			System.out.println("hello");
+		}
+	}
+
+}
